1aa6f285da62f6c29bf089baab66721c006432df
hereisraoao <neeraanakfulton@gmail.com>
2025-05-20 07:54:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🩷 Greetings! welcome to my first app :3")
+st.title("🩷 Greetings! welcome to my very first app <3")
 st.write(
     "Because this was my first app, Let me introduce myself first so you can know me better! Im Daneera Meijandini Ariestya from X-F :D"
 )
